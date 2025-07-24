c3fc2e03a7bfdba18f0ca275d8bea50fd6ed6fe7
Liston-Kaband0 <listonmacharia9@gmail.com>
2025-07-25 00:22:37 +0300

    first commit

---
+++ datastructures.py
@@ -0,0 +1,21 @@
+# Creating a list of integers
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+# Output: [1, 2, 3, 4, 5]
+numbers.extend( [6, 7, 8])
+print(numbers)
+numbers.pop()
+
+numbersa = [1, 2, 3, 4]
+numbersb = [5, 6, 7, 8]
+numbersa.append(numbersb)
+print ("numbers a")
+numbers.pop()
+
+# Creating a list of integers
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+# Output: [1, 2, 3, 4, 5]
+numbers.extend( [6, 7, 8])
+print(numbers)
+numbers.pop()
